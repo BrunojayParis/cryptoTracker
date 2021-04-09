26ec269423c0265787ce9594547beabcb23326ca
Bruno Paris <brunojparisz@gmail.com>
2021-04-09 19:37:41 -0300

    new responsive layout

--- src/App.js
+++ src/App.js
@@ -13,51 +13,50 @@ function App() {
 
   const [coins, setCoins] = useState([])
   const [state, setState] = useState('')
-  
+
   useEffect(() => {
     axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=ars&order=market_cap_desc&per_page=100&page=1&sparkline=false")
-  .then(function (response) {
-    // handle success
-    setCoins(response.data);
-    console.log(setCoins)
-  })
-  .catch(function (error) {
-    // handle error
-    document.alert(error);
-  })
-  },[]);
+      .then(function (response) {
+        // handle success
+        setCoins(response.data);
+        console.log(setCoins)
+      })
+      .catch(function (error) {
+        // handle error
+        document.alert(error);
+      })
+  }, []);
 
   //functions
-  const searchHandler =(e) =>{
+  const searchHandler = (e) => {
     setState(e.target.value);
   }
 
-  const filteredCoins = coins.filter(coin=> coin.name.toLowerCase().includes(state.toLowerCase()))
+  const filteredCoins = coins.filter(coin => coin.name.toLowerCase().includes(state.toLowerCase()))
 
 
   return (
     <div>
       <h1 class="text-4xl ">Crypto Tracker</h1>
-      <form class= "py-4">
-        <input class="border-2 border-black-200" type="text" onChange={searchHandler} label="search" />
+      <form class="py-4">
+        <input class="border-2 border-black-200" type="text" onChange={searchHandler} placeholder="search" />
+        
       </form>
-      <div class="grid gird-col-">
+      <div className="flex flex-wrap">
 
+        {filteredCoins.map(coins =>
+          <Coins
+            key={coins.id}
+            name={coins.name}
+            image={coins.image}
+            symbol={coins.symbol}
+            price={coins.current_price}
+            volume={coins.total_volume}
+            priceChange={coins.price_change_percentage_24h} />)}
       </div>
 
-      {filteredCoins.map(coins =>
-         <Coins 
-         key={coins.id} 
-         name= {coins.name} 
-         image={coins.image} 
-         symbol= {coins.symbol} 
-         price ={coins.current_price}
-         volume= {coins.total_volume}
-         priceChange= {coins.price_change_percentage_24h} />)}
-
-     
     </div>
-  );  
+  );
 }
 
 export default App;
--- src/components/Coins.js
+++ src/components/Coins.js
@@ -1,24 +1,26 @@
 import React from 'react';
 
-const Coins = ({name, image, symbol, price, volume, priceChange}) =>{
+const Coins = ({ name, image, symbol, price, volume, priceChange }) => {
 
 
 
 
-    return(
-        <div class="grid grid-cols-2 gap-2 py-2">       
-            <div class="flex" >
-                <img class="h-8" src={image} alt=""/>
-                <h1 class="px-2">{name}</h1>
-            </div>
-            <div class="grid grid-cols-4 gap-4">
-                <p className="coinSymbol">{symbol}</p>
-                <p className="coinPrice">$ {price}</p>
-                <p>{volume}</p>
-                <p>{priceChange.toFixed(2)}%</p>
+    return (
+        <div class="py-3 px-3">
+            <div className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white flex flex-col justify-center items-center  hover:shadow-xl transition-shadow duration-300 ease-in-out text-2xl">
+                <div class="px-4 py-3">
+                    <div className="flex flex-col items-center">
+                    <img class="h-20" src={image} alt="" />
+                    <h1 class="px-2">{name}</h1>
+                    </div>
+                    <p className="">{symbol}</p>
+                    <p className="">$ {price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                    <p>$ {volume.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                    <p>{priceChange.toFixed(2)}%</p>
+                </div>
             </div>
         </div>
-        
+
     )
 }
 
